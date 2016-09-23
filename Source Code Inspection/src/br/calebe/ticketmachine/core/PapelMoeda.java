74c618b14f9767d035f1f50c2fd4ea7751bce771
31502970 <31502970@089787-P33S09.macklabs.br>
2016-09-23 20:49:13 -0300

    teste

--- Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoeda.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoeda.java
@@ -7,18 +7,13 @@ package br.calebe.ticketmachine.core;
 public class PapelMoeda {
 
     protected int valor;
-    protected int quantidade;
 
-    public PapelMoeda(int valor, int quantidade) {
+    public PapelMoeda(int valor) {
         this.valor = valor;
-        this.quantidade = quantidade;
     }
 
     public int getValor() {
         return valor;
     }
 
-    public int getQuantidade() {
-        return quantidade;
-    }
 }
